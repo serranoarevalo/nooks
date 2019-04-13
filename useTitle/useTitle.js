2aff0b34ebcbdf0215038163cbd3e6a72d4a8b70
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-04-13 18:29:01 +0900

    useTitle

---
+++ useTitle/useTitle.js
@@ -0,0 +1,11 @@
+export function useTitle(initialTitle) {
+  const [title, setTitle] = useState(initialTitle);
+  function updateTitle() {
+    const titleHtml = document.querySelector("title");
+    titleHtml.innerText = title;
+  }
+  useEffect(() => {
+    updateTitle();
+  }, [title]);
+  return setTitle;
+}
